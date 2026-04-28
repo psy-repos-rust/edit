psy-repos-rust/edit

commit e5f0c4574871f5a351e3bac44ddfddf07e3ec479

Stop moving focus into the search box (#821)
This just annoyed me and I remember people complaining about it.

crates/edit/src/bin/edit/documents.rs
@@ -282,7 +282,6 @@ impl DocumentManager {
         let buffer = TextBuffer::new_rc(false)?;
         {
             let mut tb = buffer.borrow_mut();
-            tb.set_insert_final_newline(!cfg!(windows)); // As mandated by POSIX.
             tb.set_margin_enabled(true);
             tb.set_line_highlight_enabled(true);
         }

crates/edit/src/buffer/mod.rs
@@ -316,7 +316,7 @@ impl TextBuffer {
             ruler: 0,
             encoding: "UTF-8",
             newlines_are_crlf: cfg!(windows), // Windows users want CRLF
-            insert_final_newline: false,
+            insert_final_newline: !cfg!(windows), // As mandated by POSIX.
             overtype: false,
 
             wants_cursor_visibility: false,

crates/edit/src/bin/edit/draw_editor.rs
@@ -151,8 +151,6 @@ fn draw_search(ctx: &mut Context, state: &mut State) {
 
             if change {
                 action = change_action;
-                state.wants_search.focus = true;
-                ctx.needs_rerender();
             }
         }
         ctx.table_end();

commit d3f86975dc3c1298bf7300dbdf409a1df8d8b2b7

Fix text search on *nix (#824)
This regressed in #821.

crates/edit/src/bin/edit/documents.rs
@@ -282,6 +282,7 @@ impl DocumentManager {
         let buffer = TextBuffer::new_rc(false)?;
         {
             let mut tb = buffer.borrow_mut();
+            tb.set_insert_final_newline(!cfg!(windows)); // As mandated by POSIX.
             tb.set_margin_enabled(true);
             tb.set_line_highlight_enabled(true);
         }

crates/edit/src/buffer/mod.rs
@@ -316,7 +316,7 @@ impl TextBuffer {
             ruler: 0,
             encoding: "UTF-8",
             newlines_are_crlf: cfg!(windows), // Windows users want CRLF
-            insert_final_newline: !cfg!(windows), // As mandated by POSIX.
+            insert_final_newline: false, // NOTE: Even with POSIX, single-line buffers need this to be false
             overtype: false,
 
             wants_cursor_visibility: false,